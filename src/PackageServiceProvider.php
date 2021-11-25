33ea4472270de80e01faa59dcb285793e67581ab
MAAK-2904 <67573454+MAAK-2904@users.noreply.github.com>
2021-11-25 23:50:28 +0300

    Add files via upload

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MAAK\PersianLang;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/fa' => base_path('resources/lang')
+        ]);
+
+    }
+}

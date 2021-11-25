b3407a013a8a7ff812dd329d975ea6dcbb5cc39b
MAAK-2904 <67573454+MAAK-2904@users.noreply.github.com>
2021-11-26 00:38:56 +0330

    Update PackageServiceProvider.php

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -9,7 +9,7 @@ class PackageServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/fa' => base_path('resources/lang')
+            __DIR__ . '/fa' => base_path('resources/lang/fa')
         ]);
 
     }
